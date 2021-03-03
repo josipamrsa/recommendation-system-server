268f72513c9c9e0662ae015ae2b16459770ef454
Josipa Mrša <jmrsa@pmfst.hr>
2021-03-03 09:56:13 +0100

    removing unnecessary packages

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //----KONFIGURACIJA----//
 const express = require('express');
-require('express-async-errors');
+/* require('express-async-errors'); */
 const app = express();
 
 //----CONTROLLERI----//
